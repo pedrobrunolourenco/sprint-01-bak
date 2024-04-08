ed3e7d7b1d6da7cbb0d2bd4c96bb64e806010847
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-04-08 10:58:39 -0300

    commit-018

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # sprint-01-bak
  BackEnd Python para um MVP de Arvore Genealogica
+
+##
+O objetivo da Api é estruturar uma cadeia hierárquica para uma árvore genealógica,
+através de endpoints GET, POST, PUT e DELETE.
+
+O projeto foi desenvolvido em Python e banco de dados SQLLite fazendo uso do 
+conhecimento adquirido no curso. Consiste em apenas uma tabela, que faz uso de 
+um AUTO-REELACIONAMENTO, técnica comumente usada em modelagem 
+de dados para estruturar informações hierárquicas ou de árvore,  
+como em uma estrutura de árvore genealógica, organograma de uma empresa, 
+ou categorias em uma hierarquia.
+
+## Como executar 
+Será necessário ter todas as libs python listadas no `requirementos.txt` instaladas.
+Após clonar o repositório, é necessário ir ao diretório raiz, pelo terminal, para poder executar os comandos descritos abaixo.
+
+É fortemente indicado o uso de ambientes virtuais do tipo [virtualenv](https://virtualenv.pypa.io/en/latest/installation.html).
+
+```
+(env)$ pip install -r requirementos.txt
+```
+
+Este comando instala as dependências/bibliotecas, descritas no arquivo `requirementos.txt`.
+
+Para executar a API  basta executar:
+
+```
+(env)$ flask run --host 0.0.0.0 --port 5000
+```
+
+```
+(env)$ flask run --host 0.0.0.0 --port 5000 --reload
+```
+para rodar o projeto:
+http://127.0.0.1:5000
+http://10.81.234.195:5000
+
+Abra o [http://localhost:5000/#/](http://localhost:5000/#/) no navegador para verificar o status da API em execução.
--- app.py
+++ app.py
@@ -40,6 +40,26 @@ def  verifica_se_ja_existe_membro_base(nome: str) -> str :
     except Exception as e:
         logger.warning(e)
             
+def verifica_se_ja_existe_membro_base_alteracao(nome: str, id: int) -> str:
+    session = Session()
+    try:
+        Pai = aliased(Membro)
+        Mae = aliased(Membro)
+
+        membro01 = session.query(Membro, Pai.nome.label('nome_pai'), Mae.nome.label('nome_mae')) \
+                    .outerjoin(Pai, Membro.pai == Pai.id) \
+                    .outerjoin(Mae, Membro.mae == Mae.id) \
+                    .filter(func.lower(Membro.nome) == nome.lower(), Membro.id_base == 0, Membro.id != id) \
+                    .first()
+
+        if membro01 :
+          return True
+        else:
+          return False
+    except Exception as e:
+        logger.warning(e)
+
+
 
 def busca_membros_comuns(id_base) -> List[MembroViewModel] :
     session = Session()    
@@ -147,7 +167,7 @@ def obter_por_id(query: MembroGetSchemaId):
           return {"message": error_msg}, 200
         
         result = []
-        membro, pai_nome, mae_nome = membros  # Descompactando a tupla retornada
+        membro, pai_nome, mae_nome = membros  
         membro_dict = {
             "id": membro.id,
             "id_base": membro.id_base,
@@ -257,11 +277,15 @@ def altera_membro_comum_pai(form: MembroAlteraPaiSchema):
     """
 
     try:
-        session = Session()
-        stmt01 = update(Membro).where(Membro.id == form.id_pai).values(nome=form.nome)
-        session.execute(stmt01)
-        session.commit() 
-        return {"sucesso": True}, 200
+       jaexiste = verifica_se_ja_existe_membro_base_alteracao(form.nome, form.id_pai)
+       if jaexiste == True :
+          return {"sucesso": False, "mensagem": "Já "}, 404
+       else :            
+          session = Session()
+          stmt01 = update(Membro).where(Membro.id == form.id_pai).values(nome=form.nome)
+          session.execute(stmt01)
+          session.commit() 
+          return {"sucesso": True}, 200
     except Exception as e:
         logger.warning("Erro ao alterar um membro")
         return {"message": "Erro ao alterar um membro"}, 400
